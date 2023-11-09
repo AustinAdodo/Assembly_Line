aa488127559f5dc44f3760fb54014932d90459bd
Odia Adodo <subzbelow@gmail.com>
2023-11-09 11:26:39 +0100

    Added Line module and Component

---
+++ src/app/assemblyline/assemblyline.component.ts
@@ -0,0 +1,57 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'assemblyline',
+  templateUrl: './assemblyline.component.html',
+  styleUrls: ['./assemblyline.component.css']
+})
+export class AssemblyLineComponent implements OnInit {
+  @Input() stages: string[];
+  items: { [key: string]: string }[];
+  newItem: string = '';
+
+  ngOnInit() {
+    if (!this.stages) {
+      throw new Error('Input "stages" is required.');
+    }
+
+    this.items = [];
+    for (let i = 0; i < this.stages.length; i++) {
+      const item = {};
+      item[this.stages[0]] = '';
+      this.items.push(item);
+    }
+  }
+
+  addItem() {
+    if (this.newItem) {
+      this.items[0][this.stages[0]] = this.newItem;
+      this.newItem = '';
+    }
+  }
+
+  moveItem(item: { [key: string]: string }, currentIndex: number, targetIndex: number, forward: boolean) {
+    if (!this.stages) {
+      throw new Error('Input "stages" is required.');
+    }
+
+    const currentStage = this.stages[currentIndex];
+    const targetStage = this.stages[targetIndex];
+
+    if (forward) {
+      if (targetIndex < this.stages.length - 1) {
+        item[targetStage] = item[currentStage] || '';
+        delete item[currentStage];
+      } else {
+
+        this.items = this.items.filter((_, index) => index !== currentIndex);
+      }
+    } else {
+      if (targetIndex > 0) {
+        item[targetStage] = item[currentStage] || '';
+        delete item[currentStage];
+      }
+    }
+  }
+}
+
---
+++ src/app/assemblyline/assemblyline.module.ts
@@ -0,0 +1,11 @@
+import { CommonModule } from "@angular/common";
+import { NgModule } from "@angular/core";
+import { AssemblyLineComponent } from "./assemblyline.component";
+import { FormsModule } from '@angular/forms';
+
+@NgModule({
+  declarations: [AssemblyLineComponent],
+  imports: [CommonModule, FormsModule],
+  exports: [AssemblyLineComponent]
+})
+export class AssemblyLineModule {}
