6d89ea3a53c37884d444197a094f91b21859e137
Odia Adodo <subzbelow@gmail.com>
2023-11-17 22:16:41 +0100

    Update

--- __tests__/dynamicresponse.spec.ts
+++ __tests__/dynamicresponse.spec.ts
@@ -1,10 +1,10 @@
-//npm i --save-dev @types/node
 //import { promises as fsPromises } from 'fs';
 const fsPromises = require("fs").promises;
 
 /**
  * NB: In TypeScript, using require for importing modules from Node.js might cause issues,
  * especially when using TypeScript's ES modules.
+ * use npm i --save-dev @types/node then add "node" to @types in tsconfig
  * @params
  *
  */
