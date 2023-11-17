90e256f2fed626b6e224476e277a9784813a4540
Odia Adodo <subzbelow@gmail.com>
2023-11-17 13:01:47 +0100

    Update

--- jest.config.ts
+++ jest.config.ts
@@ -1,3 +1,14 @@
+//npm install --save-dev jest-environment-jsdom
+/**
+ * This environment is essential for running Jest tests in a simulated DOM environment similar to a browser
+ * @params
+ * * stages?: string[]. The amount of columns to be traversed.
+ * * items?: string[]. The total number of items.
+ *
+ * @returns
+ * A Configuration object.
+ */
+
 export default {
   testEnvironment: 'jsdom',
   module: 'ts-node/register',
