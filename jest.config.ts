3482b86817b2071c0181c9fa92b03b5b6c6d7a35
Odia Adodo <subzbelow@gmail.com>
2023-11-22 17:12:07 +0100

    Update

--- jest.config.ts
+++ jest.config.ts
@@ -1,5 +1,5 @@
 /**
- * install test environment for dom withnpm install --save-dev jest-environment-jsdom
+ * install test environment for dom with npm install --save-dev jest-environment-jsdom
  * npm install --save-dev ts-node
  * This environment is essential for running Jest tests in a simulated DOM environment similar to a browser
  * @params
