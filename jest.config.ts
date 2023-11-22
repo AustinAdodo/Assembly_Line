55545cb6cb6f4885006999cbbcf9ba0ba2f1af30
Odia Adodo <subzbelow@gmail.com>
2023-11-22 17:13:12 +0100

    update documentation for jest configuration

--- jest.config.ts
+++ jest.config.ts
@@ -1,10 +1,11 @@
 /**
+ * A Configuration object
+ *
  * install test environment for dom with npm install --save-dev jest-environment-jsdom
  * npm install --save-dev ts-node
  * This environment is essential for running Jest tests in a simulated DOM environment similar to a browser
  * @params
- * @returns
- * A Configuration object.
+ * @returns Configurations for jest
  */
 
 export default {
