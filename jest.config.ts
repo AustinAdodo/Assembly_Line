23569fe5156715ea54d0589bc4e824b90fe4a32f
Odia Adodo <subzbelow@gmail.com>
2023-11-23 14:54:19 +0100

    Update

--- jest.config.ts
+++ jest.config.ts
@@ -4,12 +4,30 @@
  * install test environment for dom with npm install --save-dev jest-environment-jsdom
  * npm install --save-dev ts-node
  * This environment is essential for running Jest tests in a simulated DOM environment similar to a browser
- * @params
+ *
+ * @params extensionsToTreatAsEsm: Defines file extensions treated as ECMAScript Modules.
+ * @params globals with ts-jest options: Enables ESM support for ts-jest.
+ * @params transform configuration for TypeScript files using ts-jest.
+ * @params moduleNameMapper: Helps Jest handle relative imports correctly.
  * @returns Configurations for jest
  */
 
 export default {
   testEnvironment: 'jsdom',
   preset: 'ts-jest',
+  extensionsToTreatAsEsm: ['.ts', '.tsx', '.jsx'],
+  transform: {
+    '^.+\\.(ts|tsx)$': 'ts-jest',
+  },
+  moduleNameMapper: {
+    '^(\\.{1,2}/.*)\\.js$': '$1',
+  },
+  globals: {
+    'ts-jest': {
+      useESM: true,
+      // Other ts-jest configuration options can be added here if needed
+    },
+  },
 };
+
 
