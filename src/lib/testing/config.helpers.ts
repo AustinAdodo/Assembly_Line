caff903247492dc149680d4cff9de90f4df7acfc
Odia Adodo <subzbelow@gmail.com>
2023-11-25 23:52:17 +0100

    Update

--- src/lib/testing/config.helpers.ts
+++ src/lib/testing/config.helpers.ts
@@ -1,5 +1,15 @@
 import { TestBed, TestBedStatic } from '@angular/core/testing';
 
+/**
+ * @params
+ * * stages?:
+ *
+ * @returns
+ *
+ * @throws
+ * Testbed compiler configuration error.
+ */
+
 type CompilerOptions = Partial<{
   providers: any[];
   useJit: boolean;
